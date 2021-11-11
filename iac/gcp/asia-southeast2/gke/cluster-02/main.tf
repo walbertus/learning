526224a6e9205be3c59750a2a5635d174834d5ab
William Albertus Dembo <29192168+walbertus@users.noreply.github.com>
2021-11-11 21:01:23 +0700

    fix: reconfigure gke cluster
    Signed-off-by: William Albertus Dembo <29192168+walbertus@users.noreply.github.com>

--- iac/gcp/asia-southeast2/gke/cluster-02/main.tf
+++ iac/gcp/asia-southeast2/gke/cluster-02/main.tf
@@ -1,31 +1,37 @@
 locals {
   cluster_name = "cluster-02"
   location     = "${var.region}-b"
-  network      = "main"
-  subnetwork   = "asia-southeast2"
+  machine_type = "e2-medium"
+  oauth_scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring",
+    "https://www.googleapis.com/auth/devstorage.read_only",
+    "https://www.googleapis.com/auth/compute"
+  ]
+  cluster_ipv4_cidr_block  = "10.102.0.0/17"
+  services_ipv4_cidr_block = "10.102.128.0/17"
+  master_ipv4_cidr_block   = "10.202.0.0/28"
 }
 
-resource "google_container_cluster" "cluster_02" {
+resource "google_container_cluster" "cluster_01" {
   provider   = google-beta
   name       = local.cluster_name
   location   = local.location
-  network    = local.network
-  subnetwork = local.subnetwork
+  network    = "main"
+  subnetwork = "asia-southeast2"
 
   default_max_pods_per_node = 100
 
   ip_allocation_policy {
-    cluster_ipv4_cidr_block  = "10.102.0.0/17"
-    services_ipv4_cidr_block = "10.102.128.0/17"
+    cluster_ipv4_cidr_block  = local.cluster_ipv4_cidr_block
+    services_ipv4_cidr_block = local.services_ipv4_cidr_block
   }
 
-  cluster_autoscaling {
-    enabled = false
-  }
   private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = false
-    master_ipv4_cidr_block  = "10.202.0.0/28"
+    master_ipv4_cidr_block  = local.master_ipv4_cidr_block
   }
 
   addons_config {
@@ -40,6 +46,11 @@ resource "google_container_cluster" "cluster_02" {
 
   initial_node_count       = 1
   remove_default_node_pool = true
+  lifecycle {
+    ignore_changes = [
+      node_config
+    ]
+  }
   node_config {
     preemptible  = true
     machine_type = "e2-micro"
@@ -63,25 +74,30 @@ resource "google_container_cluster" "cluster_02" {
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name              = "${local.cluster_name}-np-01"
   location          = local.location
-  cluster           = google_container_cluster.cluster_02.name
-  node_count        = 1
+  cluster           = google_container_cluster.cluster_01.name
   max_pods_per_node = 100
+
+  initial_node_count = 1
+  lifecycle {
+    ignore_changes = [
+      initial_node_count
+    ]
+  }
 
   node_config {
     preemptible  = true
-    machine_type = "e2-micro"
+    machine_type = local.machine_type
     disk_size_gb = 10
     disk_type    = "pd-standard"
     image_type   = "COS_CONTAINERD"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.cluster_sa.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/compute"
-    ]
+    oauth_scopes    = local.oauth_scopes
+  }
+
+  autoscaling {
+    max_node_count = 3
+    min_node_count = 1
   }
 }
